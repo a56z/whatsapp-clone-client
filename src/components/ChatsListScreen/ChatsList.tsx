c9bac25d700a69dad49b0131d42c47a6b6ca4c12
Enrique Folte <hhfolte@gmail.com>
2019-11-01 09:00:15 +0100

    caching with Apollo-Client

--- src/components/ChatsListScreen/ChatsList.tsx
+++ src/components/ChatsListScreen/ChatsList.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import moment from 'moment';
 import { List, ListItem } from '@material-ui/core';
 import styled from 'styled-components';
-import { useState, useMemo } from 'react';
+import { useCallback } from 'react';
+import { History } from 'history';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
 
 const Container = styled.div`
   height: calc(100% - 56px);
@@ -56,7 +59,7 @@ const MessageDate = styled.div`
   font-size: 13px;
 `;
 
-const getChatsQuery = `
+export const getChatsQuery = gql`
   query GetChats {
     chats {
       id
@@ -71,34 +74,46 @@ const getChatsQuery = `
   }
 `;
 
-const ChatsList = () => {
-  const [chats, setChats] = useState<any[]>([]);
-  
-  useMemo(async () => {
-    const body = await fetch(`${process.env.REACT_APP_SERVER_URL}/graphql`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ query: getChatsQuery }),
-        });
-        const {
-          data: { chats },
-        } = await body.json();
-    setChats(chats);
-  }, []);
+  interface ChatsListProps {
+    history: History;
+  }
+  const ChatsList: React.FC<ChatsListProps> = ({ history }) => {
+    const { data } = useQuery<any>(getChatsQuery);
+
+  const navToChat = useCallback(
+    chat => {
+      history.push(`chats/${chat.id}`);
+    },
+    [history]
+  );
+
+  if (data === undefined || data.chats === undefined) {
+    return null;
+  }
+  let chats = data.chats;
+
   return (
     <Container>
       <StyledList>
-        {chats.map(chat => (
-          <StyledListItem key={chat!.id} button>
-            <ChatPicture src={chat.picture} alt="Profile" />
+        {chats.map((chat: any) => (
+          <StyledListItem
+            key={chat.id}
+            data-testid="chat"
+            button
+            onClick={navToChat.bind(null, chat)}>
+            <ChatPicture
+              data-testid="picture"
+              src={chat.picture}
+              alt="Profile"
+            />
             <ChatInfo>
-              <ChatName>{chat.name}</ChatName>
+              <ChatName data-testid="name">{chat.name}</ChatName>
               {chat.lastMessage && (
                 <React.Fragment>
-                  <MessageContent>{chat.lastMessage.content}</MessageContent>
-                  <MessageDate>
+                    <MessageContent data-testid="content">
+                      {chat.lastMessage.content}
+                    </MessageContent>
+                    <MessageDate data-testid="date">
                     {moment(chat.lastMessage.createdAt).format('HH:mm')}
                   </MessageDate>
                 </React.Fragment>
